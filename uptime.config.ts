97482c2f3b6870dd023f680005befb1eba3a8905
rinex20 <rinex20@me.com>
2024-05-08 12:25:36 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,10 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "rinex20's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.site/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.site', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/rinex20', label: 'GitHub' },
+    { link: 'mailto:aa@aaa.site', label: 'Email Me', highlight: true },
   ],
 }
 
